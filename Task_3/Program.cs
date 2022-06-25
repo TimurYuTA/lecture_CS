2558a961acf069515a01633e714a2deab947b764
Timur <timuryuta@gmail.com>
2022-06-25 20:40:44 +0300

    Добавил ввод чисел пользователем

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Ввод 2-х чисел и вывод их суммы
+// int number1 = 5;
+// int number2 = 12;
+// Console.Write(number1 + number2);
+
+Console.Write("Pleas, enter first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Pleas, enter second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("SUM = ");
+Console.Write(number1 + number2);
