8eeaa84d4460a23a1be3d1f372b9d0a9b6123124
Timur <timuryuta@gmail.com>
2022-06-25 17:28:06 +0300

    прога_2

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// My first programm "Hello, World
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Welcome to C#!");
+
+// Му second programm "Hello, user!"
+Console.Write ("Please, enter your name: ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);
