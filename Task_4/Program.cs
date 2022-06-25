116a2113d14a1acbcc08f193c0775dfb1dde1626
Timur <timuryuta@gmail.com>
2022-06-25 21:04:52 +0300

    Div num1 and num2

---
+++ Task_4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Ввод двух чисел и вывод их деления
+Console.Write("Pleas, enter first number1: ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Pleas, enter first number2: ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("DIV = ");
+Console.Write(number1 / number2);
