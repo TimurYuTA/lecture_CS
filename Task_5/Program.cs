9755a3f7266d3e0103ef511211f14b84b21337a1
Timur <timuryuta@gmail.com>
2022-06-25 22:17:35 +0300

    masha check

---
+++ Task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Проверка Маша
+
+Console.Write("Please, enter name: ");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, it is Masha");
+}
+else
+{
+    Console.WriteLine("Hello, " + name);
+}
